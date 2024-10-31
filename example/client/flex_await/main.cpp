db49e64b01f60b8e836a5aa052ed977b4dc4a9fb
Mohammad Nejati <ashtumashtum@gmail.com>
2024-10-31 05:05:48 +0000

    `flex_await` supports program options

--- example/client/flex_await/main.cpp
+++ example/client/flex_await/main.cpp
@@ -11,9 +11,11 @@
 #include <boost/asio/ssl.hpp>
 #include <boost/http_io.hpp>
 #include <boost/http_proto.hpp>
+#include <boost/program_options.hpp>
 #include <boost/url.hpp>
 
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 
 #if defined(BOOST_ASIO_HAS_CO_AWAIT)
@@ -24,8 +26,15 @@ namespace asio       = boost::asio;
 namespace core       = boost::core;
 namespace http_io    = boost::http_io;
 namespace http_proto = boost::http_proto;
+namespace po         = boost::program_options;
 namespace ssl        = boost::asio::ssl;
 namespace urls       = boost::urls;
+
+#ifdef BOOST_HTTP_PROTO_HAS_ZLIB
+inline const bool http_proto_has_zlib = true;
+#else
+inline const bool http_proto_has_zlib = false;
+#endif
 
 class any_stream
 {
@@ -34,12 +43,12 @@ public:
     using plain_stream_type = asio::ip::tcp::socket;
     using ssl_stream_type   = ssl::stream<plain_stream_type>;
 
-    any_stream(plain_stream_type stream)
+    explicit any_stream(plain_stream_type stream)
         : stream_{ std::move(stream) }
     {
     }
 
-    any_stream(ssl_stream_type stream)
+    explicit any_stream(ssl_stream_type stream)
         : stream_{ std::move(stream) }
     {
     }
@@ -129,9 +138,8 @@ public:
                     std::visit(
                         [&](auto& s)
                         {
-                            if constexpr(std::is_same_v<
-                                             decltype(s),
-                                             ssl_stream_type&>)
+                            if constexpr(
+                                std::is_same_v<decltype(s),ssl_stream_type&>)
                             {
                                 s.async_shutdown(std::move(self));
                             }
@@ -153,22 +161,47 @@ private:
     std::variant<plain_stream_type, ssl_stream_type> stream_;
 };
 
-asio::awaitable<any_stream>
-connect(
-    ssl::context& ssl_ctx,
-    core::string_view host,
-    core::string_view service)
+class output_stream
 {
-    auto exec     = co_await asio::this_coro::executor;
-    auto resolver = asio::ip::tcp::resolver{ exec };
-    auto rresults = co_await resolver.async_resolve(host, service);
+    std::ofstream file_;
 
-    if(service == "https")
+public:
+    output_stream() = default;
+
+    explicit output_stream(core::string_view path)
     {
-        auto stream = ssl::stream<asio::ip::tcp::socket>{ exec, ssl_ctx };
+        file_.exceptions(std::ofstream::badbit);
+        file_.open(path, std::ios::binary);
+        if(!file_.is_open())
+            throw std::runtime_error{ "Couldn't open the output file" };
+    }
+
+    void
+    write(auto buf)
+    {
+        if(file_.is_open())
+        {
+            file_.write(static_cast<const char*>(buf.data()), buf.size());
+            return;
+        }
+        std::cout.write(static_cast<const char*>(buf.data()), buf.size());
+    }
+};
+
+asio::awaitable<any_stream>
+connect(ssl::context& ssl_ctx, urls::url_view url)
+{
+    auto executor = co_await asio::this_coro::executor;
+    auto resolver = asio::ip::tcp::resolver{ executor };
+    auto service  = url.has_port() ? url.port() : url.scheme();
+    auto rresults = co_await resolver.async_resolve(url.host(), service);
+
+    if(url.scheme() == "https")
+    {
+        auto stream = ssl::stream<asio::ip::tcp::socket>{ executor, ssl_ctx };
         co_await asio::async_connect(stream.lowest_layer(), rresults);
 
-        if(auto host_s = std::string{ host };
+        if(auto host_s = std::string{ url.host() };
            !SSL_set_tlsext_host_name(stream.native_handle(), host_s.c_str()))
         {
             throw boost::system::system_error(
@@ -180,117 +213,202 @@ connect(
         co_return stream;
     }
 
-    auto stream = asio::ip::tcp::socket{ exec };
+    auto stream = asio::ip::tcp::socket{ executor };
     co_await asio::async_connect(stream, rresults);
     co_return stream;
 }
 
-bool
-is_redirect(http_proto::response_view resp) noexcept
+auto
+is_redirect(http_proto::status status) noexcept
 {
-    switch(resp.status())
+    struct result_t
+    {
+        bool is_redirect;
+        bool need_method_change;
+    };
+
+    // The specifications do not intend for 301 and 302 redirects to change the
+    // HTTP method, but most user agents do change the method in practice.
+    switch(status)
     {
         case http_proto::status::moved_permanently:
         case http_proto::status::found:
+        case http_proto::status::see_other:
+            return result_t{ true, true };
         case http_proto::status::temporary_redirect:
         case http_proto::status::permanent_redirect:
-            return true;
+            return result_t{ true, false };
         default:
-            return false;
+            return result_t{ false, false };
     }
 }
 
-asio::awaitable<void>
-write_get_request(
-    http_proto::context& http_proto_ctx,
-    any_stream& stream,
-    core::string_view host,
-    core::string_view target)
+core::string_view
+get_target(urls::url_view url) noexcept
 {
-    auto exec    = co_await asio::this_coro::executor;
-    auto request = http_proto::request{};
+    if(url.encoded_target().empty())
+        return "/";
 
-    request.set_method(http_proto::method::get);
-    request.set_target(target);
-    request.set_version(http_proto::version::http_1_1);
-    request.set_keep_alive(false);
-    request.set(http_proto::field::host, host);
-    request.set(http_proto::field::user_agent, "Boost.Http.Io");
-
-    #ifdef BOOST_HTTP_PROTO_HAS_ZLIB
-    request.set(http_proto::field::accept_encoding, "gzip, deflate");
-    #endif
-
-    auto serializer = http_proto::serializer{ http_proto_ctx };
-    serializer.start(request);
-    co_await http_io::async_write(stream, serializer);
+    return url.encoded_target();
 }
 
-// Performs an HTTP GET and prints the response
+http_proto::request
+create_request(const po::variables_map& vm, urls::url_view url)
+{
+    using http_proto::field;
+    using http_proto::method;
+    using http_proto::version;
+
+    auto request = http_proto::request{};
+
+    request.set_method(vm.count("head") ? method::head : method::get);
+
+    if(vm.count("request"))
+        request.set_method(vm.at("request").as<std::string>());
+
+    request.set_version(
+        vm.count("http1.0") ? version::http_1_0 : version::http_1_1);
+
+    request.set_target(get_target(url));
+    request.set(field::accept, "*/*");
+    request.set(field::host, url.host());
+
+    if(vm.count("continue-at"))
+    {
+        auto value = "bytes=" +
+            std::to_string(vm.at("continue-at").as<std::uint64_t>()) + "-";
+        request.set(field::range, value);
+    }
+
+    if(vm.count("range"))
+        request.set(field::range, "bytes=" + vm.at("range").as<std::string>());
+
+    if(vm.count("user-agent"))
+    {
+        request.set(field::user_agent, vm.at("user-agent").as<std::string>());
+    }
+    else
+    {
+        request.set(field::user_agent, "Boost.Http.Io");
+    }
+
+    if(vm.count("referer"))
+        request.set(field::referer, vm.at("referer").as<std::string>());
+
+    if(vm.count("user"))
+    {
+        // TODO: use base64 encoding for basic authentication
+        request.set(field::authorization, vm.at("user").as<std::string>());
+    }
+
+    if(vm.count("compressed") && http_proto_has_zlib)
+        request.set(field::accept_encoding, "gzip, deflate");
+
+    // Set user provided headers
+    if(vm.count("header"))
+    {
+        for(auto& header : vm.at("header").as<std::vector<std::string>>())
+        {
+            if(auto pos = header.find(':'); pos != std::string::npos)
+                request.set(header.substr(0, pos), header.substr(pos + 1));
+        }
+    }
+
+    return request;
+}
+
 asio::awaitable<void>
 request(
+    const po::variables_map& vm,
+    output_stream& output,
     ssl::context& ssl_ctx,
     http_proto::context& http_proto_ctx,
-    core::string_view host,
-    core::string_view service,
-    core::string_view target)
+    http_proto::request request,
+    urls::url_view url)
 {
-    auto exec   = co_await asio::this_coro::executor;
-    auto stream = co_await connect(ssl_ctx, host, service);
+    auto stream     = co_await connect(ssl_ctx, url);
+    auto parser     = http_proto::response_parser{ http_proto_ctx };
+    auto serializer = http_proto::serializer{ http_proto_ctx };
 
-    co_await write_get_request(http_proto_ctx, stream, host, target);
+    serializer.start(request);
+    co_await http_io::async_write(stream, serializer);
 
-    auto parser = http_proto::response_parser{ http_proto_ctx };
     parser.reset();
     parser.start();
-
     co_await http_io::async_read_header(stream, parser);
 
-    while(is_redirect(parser.get()))
+    // handle redirects
+    auto referer_url = urls::url{ url };
+    for(;;)
     {
-        auto resp = parser.get();
-        if(auto it = resp.find(http_proto::field::location); it != resp.end())
-        {
-            auto url     = urls::parse_uri(it->value);
-            auto host    = url->host();
-            auto service = url->has_port() ? url->port() : url->scheme();
-            auto target  =
-                !url->encoded_target().empty() ? url->encoded_target() : "/";
+        auto [is_redirect, need_method_change] =
+            ::is_redirect(parser.get().status());
 
-            // TODO: reuse the connection when possible
+        if(!is_redirect || !vm.count("location"))
+            break;
+
+        auto response = parser.get();
+        if(auto it = response.find(http_proto::field::location);
+           it != response.end())
+        {
+            auto redirect_url = urls::parse_uri(it->value).value();
+
+            // TODO: reuse the established connection when possible
             co_await stream.async_shutdown(asio::as_tuple);
-            stream   = co_await connect(ssl_ctx, host, service);
-            co_await write_get_request(http_proto_ctx, stream, host, target);
+            stream = co_await connect(ssl_ctx, redirect_url);
+
+            // Change the method according to RFC 9110, Section 15.4.4.
+            if(need_method_change && !vm.count("head"))
+            {
+                request.set_method(http_proto::method::get);
+                // TODO: drop the request body
+            }
+            request.set_target(get_target(redirect_url));
+            request.set(http_proto::field::host, redirect_url.host());
+            request.set(http_proto::field::referer, referer_url);
+
+            referer_url = redirect_url;
+
+            serializer.reset();
+            serializer.start(request);
+            co_await http_io::async_write(stream, serializer);
 
             parser.reset();
             parser.start();
-
             co_await http_io::async_read_header(stream, parser);
         }
         else
         {
-            throw std::runtime_error{ "Bad redirect response"};
+            throw std::runtime_error{ "Bad redirect response" };
         }
     }
 
-    for(;;)
+    // stream headers
+    if(vm.count("head") || vm.count("show-headers"))
+        output.write(parser.get().buffer());
+
+    // stream body
+    if(request.method() != http_proto::method::head)
     {
-        for(auto cb : parser.pull_body())
+        for(;;)
         {
-            std::cout.write(static_cast<const char*>(
-                cb.data()), cb.size());
-            parser.consume_body(cb.size());
+            for(auto cb : parser.pull_body())
+            {
+                output.write(cb);
+                parser.consume_body(cb.size());
+            }
+
+            if(parser.is_complete())
+                break;
+
+            auto [ec, _] =
+                co_await http_io::async_read_some(stream, parser, asio::as_tuple);
+            if(ec && ec != http_proto::condition::need_more_input)
+                throw boost::system::system_error{ ec };
         }
-
-        if(parser.is_complete())
-            break;
-
-        auto [ec, _] = co_await http_io::async_read_some(
-            stream, parser, asio::as_tuple);
-        if(ec && ec != http_proto::condition::need_more_input)
-            throw boost::system::system_error{ ec };
     }
 
+    // clean shutdown
     auto [ec] = co_await stream.async_shutdown(asio::as_tuple);
     if(ec && ec != ssl::error::stream_truncated)
         throw boost::system::system_error{ ec };
@@ -301,25 +419,73 @@ main(int argc, char* argv[])
 {
     try
     {
-        if(argc != 2)
+        auto odesc = po::options_description{"Options"};
+        odesc.add_options()
+            ("help,h", "produce help message")
+            ("head,I", "Show document info only")
+            ("header,H",
+                po::value<std::vector<std::string>>()->value_name("<header>"),
+                "Pass custom header(s) to server")
+            ("location,L", "Follow redirects")
+            ("continue-at,C",
+                po::value<std::uint64_t>()->value_name("<offset>"),
+                "Resume transfer offset")
+            ("range,r",
+                po::value<std::string>()->value_name("<range>"),
+                "Retrieve only the bytes within range")
+            ("output,o",
+                po::value<std::string>()->value_name("<file>"),
+                "Write to file instead of stdout")
+            ("request,X",
+                po::value<std::string>()->value_name("<method>"),
+                "Specify request method to use")
+            ("show-headers,i", "Show response headers in the output")
+            ("referer,e",
+                po::value<std::string>()->value_name("<url>"),
+                "Referer URL")
+            ("user,u",
+                po::value<std::string>()->value_name("<user:password>"),
+                "Server user and password")
+            ("user-agent,A",
+                po::value<std::string>()->value_name("<name>"),
+                "Send User-Agent <name> to server")
+            ("url",
+                po::value<std::string>()->value_name("<url>"),
+                "URL to work with")
+            ("compressed", "Request compressed response")
+            ("http1.0", "Use HTTP 1.0");
+
+        auto podesc = po::positional_options_description{};
+        podesc.add("url", 1);
+
+        po::variables_map vm;
+        po::store(
+            po::command_line_parser{ argc, argv }
+                .options(odesc)
+                .positional(podesc)
+                .run(),
+            vm);
+        po::notify(vm);
+
+        if(vm.count("help") || !vm.count("url"))
         {
-            std::cerr << "Usage: flex_await <url>\n"
-                      << "Example:\n"
-                      << "    flex_await https://www.example.com\n";
+            std::cerr
+                << "Usage: flex_await [options...] <url>\n"
+                << "Example:\n"
+                << "    flex_await https://www.example.com\n"
+                << "    flex_await -L http://httpstat.us/301\n"
+                << odesc;
             return EXIT_FAILURE;
         }
 
-        auto url = urls::parse_uri(argv[1]);
+        auto url = urls::parse_uri(vm.at("url").as<std::string>());
         if(url.has_error())
         {
-            std::cerr << "Failed to parse URL\n"
-                      << "Error: " << url.error().what() << std::endl;
+            std::cerr
+                << "Failed to parse URL\n"
+                << "Error: " << url.error().what() << std::endl;
             return EXIT_FAILURE;
         }
-        auto host    = url->host();
-        auto service = url->has_port() ? url->port() : url->scheme();
-        auto target =
-            !url->encoded_target().empty() ? url->encoded_target() : "/";
 
         auto ioc            = asio::io_context{};
         auto ssl_ctx        = ssl::context{ ssl::context::tlsv12_client };
@@ -331,17 +497,31 @@ main(int argc, char* argv[])
             http_proto::response_parser::config cfg;
             cfg.body_limit = std::numeric_limits<std::size_t>::max();
             cfg.min_buffer = 1024 * 1024;
-            #ifdef BOOST_HTTP_PROTO_HAS_ZLIB
-            cfg.apply_gzip_decoder = true;
-            cfg.apply_deflate_decoder = true;
-            http_proto::zlib::install_service(http_proto_ctx);
-            #endif
+            if(http_proto_has_zlib)
+            {
+                cfg.apply_gzip_decoder    = true;
+                cfg.apply_deflate_decoder = true;
+                http_proto::zlib::install_service(http_proto_ctx);
+            }
             http_proto::install_parser_service(http_proto_ctx, cfg);
         }
 
+        auto output = [&]
+        {
+            if(vm.count("output"))
+                return output_stream{ vm.at("output").as<std::string>() };
+            return output_stream{};
+        }();
+
         asio::co_spawn(
             ioc,
-            request(ssl_ctx, http_proto_ctx, host, service, target),
+            request(
+                vm,
+                output,
+                ssl_ctx,
+                http_proto_ctx,
+                create_request(vm, url.value()),
+                url.value()),
             [](std::exception_ptr ep)
             {
                 if(ep)
